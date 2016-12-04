9d522ae33b604ad52af241e0700ba7731e878cb0
Daniel Pape <danielwpape@gmail.com>
2016-12-04 15:58:23 +0000

    Coded up nav header, needs styling

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -11,18 +11,28 @@ var Navigation = React.createClass({
     render: function() {
         return (
           <div className="header container">
-            <div className="col-sm-6 links">
+            <div className="col-xs-6 links">
               <Marque/>
             </div>
-            <div className="col-sm-3 links">
+            <div className="col-xs-3 links tablet">
                <p className="truncate"><strong>Email:</strong> <a href={'https://itunes.apple.com/gb/app/eve-alarm-time-to-sleep/id633081958?mt=8#'}>danielwpape@gmail.com</a></p>
                <p className="truncate"><strong>Twitter</strong> <a href={'https://twitter.com/danielpape'}>@danielpape</a></p>
                 <p className="truncate"><strong>GitHub</strong> <a href={'https://github.com/danielpape'}>/danielpape</a></p>
             </div>
-            <div className="col-sm-3 links">
+            <div className="col-xs-3 links mobile">
+               <p className="truncate"><a href={'https://itunes.apple.com/gb/app/eve-alarm-time-to-sleep/id633081958?mt=8#'}><strong>Email</strong></a></p>
+               <p className="truncate"><a href={'https://twitter.com/danielpape'}><strong>Twitter</strong></a></p>
+               <p className="truncate"><a href={'https://github.com/danielpape'}><strong>GitHub</strong></a></p>
+            </div>
+            <div className="col-xs-3 links tablet">
               <p><strong>Products:</strong> <a href={'https://itunes.apple.com/gb/app/eve-alarm-time-to-sleep/id633081958?mt=8#'}>Eve Alarm</a></p>
               <p><strong>&nbsp;</strong> <a href={'https://twitter.com/danielpape'}>Independent</a></p>
               <p><strong>&nbsp;</strong> <a href={'https://github.com/danielpape'}>Alta</a></p>
+            </div>
+            <div className="col-xs-3 links mobile">
+              <p><a href={'https://itunes.apple.com/gb/app/eve-alarm-time-to-sleep/id633081958?mt=8#'}>Eve Alarm</a></p>
+              <p><strong></strong> <a href={'https://twitter.com/danielpape'}>Independent</a></p>
+              <p><strong></strong> <a href={'https://github.com/danielpape'}>Alta</a></p>
             </div>
         </div>
         );
