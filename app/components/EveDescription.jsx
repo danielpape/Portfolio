3fa8b3b82b7ae5989f2609ec4c275de3f23d8ab0
Daniel Pape <danielwpape@gmail.com>
2017-01-02 12:33:54 +0000

    Added tiles to front page

--- app/components/EveDescription.jsx
+++ app/components/EveDescription.jsx
@@ -1,27 +1,35 @@
 var React = require('react');
 import {Image} from 'react-bootstrap';
+var {Link} = require('react-router');
 
 var EveDescription = () => {
     return (
         <section>
-            <div className="container">
+            <div className="container-fluid">
                 <div>
+                  <Link to="/eve">
                     <div className="col-xs-12 col-sm-6 col-lg-3 appTile eveTile">
                         <h3>Eve Alarm</h3>
                         <h4>An alarm app for iPhone</h4>
                     </div>
+                    </Link>
+                    <Link to="/independent">
                     <div className="col-xs-12 col-sm-6 col-lg-3 appTile indTile">
                         <h3>Independent</h3>
                         <h4>Subscriptions website</h4>
                     </div>
+                  </Link>
+                  <Link to="/alta">
                     <div className="col-xs-12 col-sm-6 col-lg-3 appTile altTile">
                         <h3>Alta</h3>
                         <h4>Landscape photography app</h4>
-
                     </div>
+                  </Link>
+                  <Link to="/eve">
                     <div className="col-xs-12 col-sm-6 col-lg-3 appTile appsTile">
                         <h3>View all apps</h3>
                     </div>
+                  </Link>
                 </div>
             </div>
         </section>
