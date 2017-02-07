05bb56ff25ce2756b0cb3ebcf2d2a63ea34d5787
Daniel Pape <danielwpape@gmail.com>
2017-02-07 22:35:40 +0000

    Added front page links and styling

--- app/components/EveDescription.jsx
+++ app/components/EveDescription.jsx
@@ -7,30 +7,30 @@ var EveDescription = () => {
       <div>
         <section className="topBottomLargeMargins appSection eveTile">
           <div className="container">
-            <div className="col-sm-4">
+            <div className="col-sm-4 col-sm-offset-2">
               <Image src={require('../images/eve_homePageImage.png')} className="width100"/>
             </div>
-              <div className="col-sm-8">
+              <div className="col-sm-4">
                   <h1><strong>Eve Alarm</strong></h1><h2>An alarm app that reminds you when to go to bed</h2>
               </div>
               </div>
         </section>
         <section className="topBottomLargeMargins appSection indTile">
           <div className="container">
-            <div className="col-sm-4">
-
+            <div className="col-sm-4 col-sm-offset-2">
+              <Image src={require('../images/eve_homePageImage.png')} className="width100"/>
             </div>
-              <div className="col-sm-8">
+              <div className="col-sm-4">
                   <h1><strong>Independent</strong></h1><h2>Subscriptions website</h2>
               </div>
               </div>
         </section>
         <section className="topBottomLargeMargins appSection altTile">
           <div className="container">
-            <div className="col-sm-4">
-
+            <div className="col-sm-4 col-sm-offset-2">
+              <Image src={require('../images/eve_homePageImage.png')} className="width100"/>
             </div>
-              <div className="col-sm-8">
+              <div className="col-sm-4">
                   <h1><strong>London Evening Standard</strong></h1><h2>Progress 1000 app</h2>
               </div>
               </div>
--- app/components/Jumbotron.jsx
+++ app/components/Jumbotron.jsx
@@ -8,8 +8,8 @@ var Jumbotron = React.createClass({
         return (
             <div className="jumbotron">
                 <div className="container topBottomLargeMargins">
-                    <div className="col-sm-12">
-                        <h1><strong>Daniel Pape</strong></h1><h2> A product designer and front end developer based in London, England.</h2>
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <h1><strong>Daniel Pape</strong>: A product designer and front end developer based in London, England.</h1>
                     </div>
                 </div>
             </div>
