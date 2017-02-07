1098fa4119d70acfa8a743400745665bd19ef203
Daniel Pape <danielwpape@gmail.com>
2017-02-07 22:18:13 +0000

    Added Progress 1000 content and edited stylesheet

--- app/components/EveningStandard.jsx
+++ app/components/EveningStandard.jsx
@@ -21,11 +21,11 @@ var EveningStandard = () => {
                 </div>
             </div>
         </section>
-        <section className="topBottomMargins">
+        <section className="topBottomMargins darkBack">
             <div className="container">
                 <div className="row">
                   <div className="col-sm-6">
-
+                    <Image src={require('../images/progress_event.jpg')} className="width100"/>
                   </div>
                   <div className="col-sm-6">
                       <h2>Background</h2>
@@ -37,28 +37,40 @@ var EveningStandard = () => {
             </div>
         </section>
         <section className="topBottomLargeMargins">
-          <div className="container">
-                <div className="appDescriptionBody">
-                  <div className="col-sm-8"><Image src={require('../images/independentsubs_interface.jpg')} className="width100"/>
-                  </div>
-                  <div className="col-sm-4">
+          <div className="container centerAlign">
+            <div className="appDescriptionBody">
+                  <div className="col-sm-8 col-sm-offset-2">
                     <h2>Software Approach</h2>
                       <p>Organising 1000 entries is a challenge to manage from a interface design and software design point of view. To optimise the bundle size both the iPhone and iPad views to use the same view controllers which I write to be GUI-neutral. Both versions of the app also use the same JSON database which contains the 1000 entries in the list.</p>
               </div>
-                </div>
+            </div>
               </div>
         </section>
         <section className="topBottomLargeMargins">
           <div className="container">
                 <div className="appDescriptionBody">
-                  <div className="col-sm-4"><Image src={require('../images/ProgressMakers_UI.png')} className="width100"/>
+                  <div className="col-sm-4 col-sm-offset-2"><Image src={require('../images/ProgressMakers_UI.png')} className="width100"/>
                   </div>
-                  <div className="col-sm-8">
+                  <div className="col-sm-4">
                     <h2>UI Design</h2>
                       <p>The app presents all 1000 entries in a collection view which is split into the different categories. Once selected, an entry is presented as a popover to improve readability. On mobile the popover takes up the whole screen. To increase engagement the entries are paginated, making it simple for the user to swipe between each page.</p>
               </div>
                 </div>
               </div>
+        </section>
+        <section className="topBottomLargeMargins colourBack">
+            <div className="container">
+                <div className="appDescriptionBody">
+                      <div className="col-sm-12">
+                        <h2>UI & Graphic Design</h2>
+                          <p>I used a design for the app which seeks to convey the modernity and understated style that defines the people that have made the list.</p>
+                      </div>
+              </div>
+            </div>
+            <div className="container-fluid topMargin">
+                  <div className="col-sm-6"><Image src={require('../images/progress_interface1.jpg')} className="width100"/></div>
+                    <div className="col-sm-6"><Image src={require('../images/progress_interface2.jpg')} className="width100"/></div>
+            </div>
         </section>
         <Footer/>
         </div>
