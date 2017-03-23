269bae36a4da03687e56b37d96d99f4d1ddacdd7
Daniel Pape <danielwpape@gmail.com>
2017-03-23 21:41:54 +0000

    Removed unused fonts

--- app/components/Eve.jsx
+++ app/components/Eve.jsx
@@ -29,7 +29,7 @@ var Eve = () => {
                           <div className="appDescriptionBody">
                             <h2>Idea</h2>
                             <blockquote>If you go to bed early and get a restful night's sleep then you shouldn't need an alarm in the morning.</blockquote>
-                            <p>This was the idea behind Eve Alarm. Most people can't choose that time at which they wake up, but they can choose when to go to bed. Eve Alarm is an alarm app that uses this thinking to flip a tradition alarm on its head: <em>a bed time alarm</em> rather than a wake time alarm.</p>
+                            <p>This was the idea behind Eve Alarm. Most people can't choose that time at which they wake up, but they can choose when to go to bed. Eve Alarm is an alarm app that uses this thinking to flip a traditional alarm on its head: <em>a bed time alarm</em> rather than a wake time alarm.</p>
                       </div>
                         </div>
                     </div>
