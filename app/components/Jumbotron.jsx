b7e8648ab934f82a00fb18640b1369d92cd12bb9
Daniel Pape <danielwpape@gmail.com>
2017-04-01 11:53:09 +0100

    Jumbotron copy & styling

--- app/components/Jumbotron.jsx
+++ app/components/Jumbotron.jsx
@@ -8,7 +8,8 @@ var Jumbotron = React.createClass({
             <div className="jumbotron">
                 <div className="container topBottomLargeMargins">
                     <div className="col-sm-12">
-                        <h1>Hello, I'm <strong>Daniel Pape</strong></h1><h2>a product designer and front end developer</h2>
+                        <h1>Hello, I'm <strong>Daniel Pape</strong></h1>
+                        <h2>I currently work as a product designer at <a target="_blank" href="http://independent.co.uk" className="independent">The Independent Newspaper</a>, specialising in user interface design, idea generation & iOS development. I develop <a target="_blank" href="https://appstore.com/danielpape" className="myOwnApps">my own iOS applications</a> in my spare time and have been featured in the App Store <em>New &amp; Noteworthy</em> and <em>What's Hot</em> sections.</h2>
                     </div>
                 </div>
             </div>
