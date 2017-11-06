a061d8a1fc53652939cc409773eafd736eb5f8d3
Daniel Pape <danielwpape@gmail.com>
2017-11-06 11:57:33 +0000

    Updated copy and styling on ES Page

--- app/components/EveningStandard.jsx
+++ app/components/EveningStandard.jsx
@@ -3,76 +3,60 @@ import {Image} from 'react-bootstrap';
 
 var EveningStandard = () => {
     return (
-      <div>
-        <section className="appsSplash">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                      <h1>London Evening Standard</h1>
-                      <h2>The Progress 1000 is an annual celebration of the most influential people in the capital</h2>
-                        <p className="topMarginSmall"><strong>Project:</strong> The Progress 1000 Event App</p>
-                        <p><strong>Role:</strong> Design & Development</p>
-                        <p><strong>Platforms:</strong> iPhone, iPad</p>
-                        <p><strong>Language:</strong> Objective-C</p>
-                        <p className="topMarginSmall"><strong>Link:</strong> <a href="https://itunes.apple.com/gb/app/progress-1000-by-london-evening/id931391607?mt=8">App Store</a></p>
+        <div>
+            <section className="appsSplash standardBG">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-8 col-sm-offset-2">
+                            <h1>London Evening Standard</h1>
+                            <p className="topMarginSmall">
+                              <strong>Project:</strong> 2017 Site Design</p>
+                              <p>
+                                <strong>Role:</strong> Lead Designer</p>
+                            <p>
+                                <strong>Link:</strong> <a href="https://standard.co.uk">standard.co.uk</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-        <section className="topBottomMargins darkBack">
-            <div className="container">
-                <div className="row">
-                  <div className="col-sm-6">
-                    <Image src={require('../images/progress_event.jpg')} className="width100"/>
-                  </div>
-                  <div className="col-sm-6">
-                      <h2>Background</h2>
-                        <p>The Progress 1000 is an annual event that is organised and curated by London Evening Standard. It celebrates the most influential people in the capital from all walks of life. I created a bespoke platform that has been used for iPhone & iPad apps over the past 3 years, which features all 1000 entries in the list.</p>
-                          <blockquote className="topMargin">There are so many inspiring, dedicated and heroic people on the Progress 1000 list. From giants of industry and successful entrepreneurs, to sporting heroes and Olympic medalists.</blockquote>
-                          <p className="centerAlign"><strong>Sadiq Khan</strong> Mayor of London</p>
+            </section>
+            <section className="topBottomMargins">
+                <div className="container">
+                        <div className="col-sm-8 col-sm-offset-2">
+                            <p>The London Evening Standard has been published since 1827 and has constantly adapted to stay relevant in the publishing industry. In 2017 the standard.co.uk underwent a replatforming where I was lead designer.</p>
+                        </div>
                 </div>
+            </section>
+            <section className="topBottomMargins">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <p>The main focus os the redesign was a change in tone of voice from reporting mainly on crime & politics to a more lifestyle and celebrity focused output. I used this change of tone to work with senior stakeholders on a 3 month project that launched in October 2017.</p>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section className="topBottomLargeMargins">
-          <div className="container centerAlign">
-            <div className="appDescriptionBody">
-                  <div className="col-sm-8 col-sm-offset-2">
-                    <h2>Information Architecture</h2>
-                      <p>Organising 1000 entries is a challenge to manage from a interface design and software architecture aspect. The information is presented to the user grouped by section, which the user can browse through from any screen in the app.   To optimise the bundle size both the iPhone and iPad views to use the same view controllers which I write to be GUI-neutral. Both versions of the app also use the same JSON database which contains the 1000 entries in the list.</p>
-              </div>
-            </div>
-              </div>
-        </section>
-        <section className="topMargin">
-          <div className="container">
-                <div className="appDescriptionBody">
-                  <div className="col-sm-6">
-                    <h2>User Interface Design</h2>
-                      <p>To allow the user to explore the entries in the list, a persistant menu button is always present in the top-left corner with a prompt to interact with it when the app is first opened. The app uses a collection view to present the entries in each category. Once selected, an entry is presented as a popover to improve readability. On mobile the popover takes up the whole screen. To increase engagement the entries are paginated, making it simple for the user to swipe between each page.</p>
-              </div>
-              <div className="col-sm-6">
-                <Image src={require('../images/progressUI.jpg')} className="width100"/>
-              </div>
+            </section>
+            <section className="topBottomMargins">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <p>The redesign also offered an opportunity to address limitations & frustrations from the editorial and commercial teams. I spent 3 weeks interviewing the people that work on the site and identified a number of issues. Because of the responsive nature of the website images were often cropped in an odd way, cropping people who were at the edges of an image for example. I designed the site so that in almost all cases images retain their original 6x4 ratio. This rule is broken to give emphasis to higher priority elements such as the highest priority “hero” component and the full width comment highlights.</p>
+                    </div>
                 </div>
-              </div>
-        </section>
-        <section className="topBottomLargeMargins colourBack">
-            <div className="container">
-                <div className="appDescriptionBody">
-                      <div className="col-sm-12">
-                        <h2>Graphic Design</h2>
-                          <p>I used a design for the app which seeks to convey the modernity and understated style that defines the people that have made the list.</p>
-                      </div>
-              </div>
-            </div>
-            <div className="container-fluid topMargin">
-                  <div className="col-sm-6"><Image src={require('../images/progress_interface1.jpg')} className="width100"/></div>
-                    <div className="col-sm-6"><Image src={require('../images/progress_interface2.jpg')} className="width100"/></div>
-            </div>
-        </section>
+            </section>
+            <section className="topBottomMargins">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <p>I designed a library of component that can be curated by editors to build up the structure of the page. These components have varying levels of information density which is intended to give the page rhythm which can be use to clearly guide the user through the page. The components are based on a 12 column grid, although the bounds of the columns are strategically broken in order to add emphasis to certain elements and to add depth. Once example of this are the blocks that appear to emphasise the on the hero elements.</p>
+                    </div>
+                </div>
+            </section>
+            <section className="topBottomMargins">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        <p>These blocks break the grid be being offset by half of the margin width vertically and horizontally. This places them partly within the margins of the design, reinforcing the floating, airy nature of the design. The parallax effect is used sparingly, but gives the same dynamic aesthetic to the comment piece highlights.</p>
+                    </div>
+                </div>
+            </section>
         </div>
-          );
+    );
 };
 
 module.exports = EveningStandard;
--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var Jumbotron = require('Jumbotron');
 var Contact = require('Contact');
 var FrontpageWork = require('FrontpageWork');
+var EveningStandard = require('EveningStandard');
 
 var Home = (props) => {
   return(
